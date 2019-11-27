6550c9d784f873d1173f40707a0cf135b5c72935
Gustavo Murayama <gmurayama@outlook.com>
2019-11-27 09:56:38 -0300

    Test compression/decompression

---
+++ src/Compression.Tests/HuffmanTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Compression.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Compression.Tests
+{
+    [TestClass]
+    public class HuffmanTests
+    {        
+        [TestMethod]
+        public void TestCompression()
+        {
+            string content = "dabdccadadad";
+            byte[] file = Encoding.UTF8.GetBytes(content);
+
+            var huffman = new Huffman();
+
+            huffman.Compress(file);
+            var compressedFile = huffman.CompressedData;
+            var bits = new BitArray(compressedFile);
+
+            Assert.AreEqual("0111000101101110110110", bits.PrintArray());
+        }
+
+        [TestMethod]
+        public void TestDecompression()
+        {
+            string content = "The quick Brown Fox jumps over the Lazy Dog";
+            byte[] file = Encoding.UTF8.GetBytes(content);
+
+            var huffman = new Huffman();
+
+            huffman.Compress(file);
+            var compressed = huffman.CompressedData;
+            var decompressed = huffman.Decompress(compressed);
+
+            string decompressedContent = Encoding.UTF8.GetString(decompressed);
+
+            Assert.AreEqual(content, decompressedContent);
+        }
+    }
+}
---
+++ src/Compression.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Compression.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Compression.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f3a1228-1eb6-4cc6-a40e-1a1f643488e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
