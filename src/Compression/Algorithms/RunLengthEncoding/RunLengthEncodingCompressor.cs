052ed092d37cd72fdbd76d8bc2ad91b315502253
Lucas Damazio Costa <lucas.damaziocosta@outlook.com>
2019-12-01 14:46:26 -0300

    feat(RunLengthEncodingCompressor): Add class and Compress method.

---
+++ src/Compression/Algorithms/RunLengthEncoding/RunLengthEncodingCompressor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compression.Algorithms.RunLengthEncoding
+{
+    class RunLengthEncodingCompressor
+    {
+        private const byte MAX_PACKAGE_SIZE = 255; 
+        public byte[] CompressFile()
+        {
+            List<byte> newFile = new List<byte>();
+
+            byte[] file = new byte[] { 1, 2, 2, 2, 1 };
+
+            byte runValue = file[0];
+            byte runCount = 1;
+            for (int position = 1; position < file.Length; position++)
+            {
+                byte currentByte = file[position];
+                if(runValue == currentByte)
+                {
+                    if(runCount != MAX_PACKAGE_SIZE)
+                        runCount++;
+                    else
+                    {
+                        newFile.Add(runCount);
+                        newFile.Add(runValue);
+                        runCount = 1;
+                    }
+                }
+                else
+                {
+                    newFile.Add(runCount);
+                    newFile.Add(runValue);
+                    runValue = currentByte;
+                    runCount = 1;
+                }
+            }
+
+            newFile.Add(runCount);
+            newFile.Add(runValue);
+
+            return newFile.ToArray();
+        }
+    }
+}
