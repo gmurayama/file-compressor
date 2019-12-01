6215c5d3c8702694dc46125c41889ad85b1bcdda
Lucas Damazio Costa <lucas.damaziocosta@outlook.com>
2019-12-01 18:44:25 -0300

    feat(FileReceiver): Add method saveFile

--- src/FileReceiver/FileReceiver.cs
+++ src/FileReceiver/FileReceiver.cs
@@ -45,12 +45,15 @@ namespace FileReceiver
                         {
                             body = reader.ReadToEnd();
                         }
-                        Console.WriteLine("ANTES DO SERIALIZE");
+                        
                         var compressed = JsonConvert.DeserializeObject<CompressedFile>(body);
-
-                        Console.WriteLine("DEPOIS DO SERIALIZE");
-                        var d = new PaintImageDelegate(PaintImage);
-                        pictureBoxImage.Invoke(d, compressed);
+                        
+                        saveFile(compressed);   
+                        if (compressed.isImage())
+                        {
+                            var d = new PaintImageDelegate(PaintImage);
+                            pictureBoxImage.Invoke(d, compressed);
+                        }
                     }
                 }
 
@@ -64,6 +67,27 @@ namespace FileReceiver
         {
             server.Stop();
             serverThread.Join(1000 * 5);
+        }
+
+        private void saveFile(CompressedFile compressedFile)
+        {
+            ICompressor compressor;
+
+            if (compressedFile.isHuffman())
+            {
+                compressor = new HuffmanCoding();
+            }
+            else
+            {
+                compressor = new RunLengthEncodingCompressor();
+            }
+
+            FileStream fileStream = new FileStream(compressedFile.OriginalFileName, FileMode.Create);
+            byte[] fileData = compressor.Decompress(compressedFile);
+
+            for (int position = 0; position < fileData.Length; position++)
+                fileStream.WriteByte(fileData[position]);
+            fileStream.Close();
         }
 
         private void PaintImage(CompressedFile compressedFile)
